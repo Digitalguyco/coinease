cf4042295dc48db95bd1899f7c040af211c5cf1b
Digitalguyco <youngkhito@gmail.com>
2025-03-15 06:23:21 +0100

    build fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,4 @@
  "use client";
-import ProtectedRoute from '../components/ProtectedRoute';
 import Dashboard from '../components/Dashboard'; // Your actual dashboard component
 
 export default function DashboardPage() {
